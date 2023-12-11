fd5c7fcee4edc546d2a711cdc480630cafe5d679
asentientbanana <asentientbanana@gmail.com>
2023-12-11 01:42:37 +0100

    ;

--- lib/controllers/archive.dart
+++ lib/controllers/archive.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:mangakolekt/controllers/types/zip.dart';
 import 'package:mangakolekt/models/book.dart';
