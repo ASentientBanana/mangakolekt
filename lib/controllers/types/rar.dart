4a24f5b175b33e4c51a83b3eace6aaf9a5fd3358
asentientbanana <asentientbanana@gmail.com>
2024-01-17 17:07:32 +0100

    random update

---
+++ lib/controllers/types/rar.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'package:mangakolekt/ffi/ffi_handler.dart';
+import 'package:mangakolekt/util/archive.dart';
+import 'package:mangakolekt/util/util.dart';
+import 'package:path/path.dart' as p;
+import 'package:flutter/material.dart';
+import 'package:mangakolekt/controllers/archive.dart';
+import 'package:mangakolekt/models/book.dart';
+import 'package:mangakolekt/constants.dart';
+import 'package:image/image.dart' as dart_img;
+
+class ZipBookController extends BaseBookController {
+  final List<String> fileTypes = ['cbr', 'rar'];
+
+  @override
+  Future<List<String>> unpackCovers(String pathToDir) async {
+    return [];
+  }
+
+  // TODO: Decouple the unzip logic from loading the books
+  @override
+  Future<void> unpack(String pathToBook, String dest) async {}
+
+  @override
+  bool checkType(String type) {
+    return fileTypes.contains(type);
+  }
+}
