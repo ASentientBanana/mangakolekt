a776e64512c42c7e8c546c6183b161d2ebe8c4f3
asentientbanana <asentientbanana@gmail.com>
2024-06-22 18:17:27 +0200

    changed the ffi service to use library namespace thing

---
+++ lib/services/ffi/utility.dart
@@ -0,0 +1,60 @@
+part of ffi_service;
+
+DynamicLibrary loadWindows() {
+  if (kReleaseMode) {
+// I'm on release mode, absolute linking
+    String pathToLib = join(Directory(Platform.resolvedExecutable).parent.path,
+        "manga_archive.dll");
+// return FFIService(dyLib: DynamicLibrary.open(pathToLib));
+    return DynamicLibrary.open(pathToLib);
+  } else {
+// I'm on debug mode, local linking
+    var path = Directory.current.path;
+    return DynamicLibrary.open(
+        join(path, 'lib', 'dev_lib', 'manga_archive.dll'));
+  }
+}
+
+DynamicLibrary loadLinux() {
+  final String path;
+  if (kReleaseMode) {
+// /home/petar/Projects/mangakolekt/dist/mangakolekt/dist/linux/mangakolekt:
+    path = join(Directory(Platform.resolvedExecutable).parent.path,
+        'lib/manga_archive.so');
+  } else {
+    path = 'lib/linux/manga_archive.so';
+  }
+  return DynamicLibrary.open(path);
+}
+
+//TODO: Add default implementation
+loadUnsupported() {
+// throw Error.safeToString("Platform unsuported");
+// Temp solution for the compiler
+  return loadLinux();
+}
+
+DynamicLibrary loadAndroid() {
+  const path = 'manga_archive.so';
+  return DynamicLibrary.open(path);
+}
+
+//
+//  loadService returns a platform specific native ffiService
+//  or a generic PlatformService
+//  for the widest implementation as a generic return
+//
+DynamicLibrary? loadService() {
+  if (Platform.isLinux) {
+    return loadLinux();
+  }
+  if (Platform.isWindows) {
+    return loadWindows();
+  }
+  if (Platform.isAndroid) {
+    return loadAndroid();
+  }
+
+  return null;
+// return UnsupportedNativePlatformService();
+}
---
+++ lib/services/ffi/zip.dart
@@ -0,0 +1,100 @@
+part of ffi_service;
+
+List<FFICoverOutputResult> ffiUnzipCovers(
+    List<String> files, String path, String out) {
+  final dyLib = loadService();
+
+  if (dyLib == null) {
+    return [];
+  }
+
+  final unzipCoversFromDir = dyLib
+      .lookupFunction<UnziperFunc, UnziperFunc>("Unzip_Covers", isLeaf: true);
+
+// TODO:
+// convert to json
+  final filesString = jsonEncode(files);
+
+// final filesString = files.join("&&");
+
+// send data to ffi
+  final Pointer<Utf8> filesStringPtr = filesString.toNativeUtf8();
+  final Pointer<Utf8> pathPtr = path.toNativeUtf8();
+  final Pointer<Utf8> outPtr = out.toNativeUtf8();
+  var res = unzipCoversFromDir(filesStringPtr.cast<Uint8>(),
+      pathPtr.cast<Uint8>(), outPtr.cast<Uint8>());
+
+  calloc.free(filesStringPtr);
+  calloc.free(pathPtr);
+  calloc.free(outPtr);
+
+// json -> List
+  final output = res.cast<Utf8>().toDartString();
+  final ffiOut = jsonDecode(output);
+
+  if (ffiOut.isEmpty) {
+    return [];
+  }
+  calloc.free(res);
+//Expected structure
+// {
+// archiveName:string
+// destinationPath:string
+//  directoryFile:string
+// }
+//map to obj
+
+  if ((ffiOut is! List)) {
+    print('FFI NOT List: ${ffiOut.runtimeType}');
+    return [];
+  }
+  final List<FFICoverOutputResult> covers = [];
+  (ffiOut as Iterable).forEach((element) {
+    if (element != null) {
+      final _cover = FFICoverOutputResult.fromMap(element);
+      if (_cover != null) {
+        covers.add(_cover);
+      }
+    }
+  });
+  print("ffi output:: $covers");
+  return covers;
+}
+
+Future<List<String>> ffiUnzipSingleBook(
+    String _bookPath, String _targetPath) async {
+  final dyLib = loadService();
+  if (dyLib == null) {
+    return [];
+  }
+  final nativeBindings = nb.NativeLibrary(dyLib);
+  final pBookPath = _bookPath.toNativeUtf8().cast<Char>();
+  final pTargetPath = _targetPath.toNativeUtf8().cast<Char>();
+
+  try {
+    nativeBindings.Unzip_Single_book(pBookPath, pTargetPath);
+// final files = pFiles.cast<Utf8>().toDartString();
+
+// final decodedList = jsonDecode(files);
+// if(decodedList is! Iterable ){
+//   return [];
+// }
+// final List<String> filesList = [];
+//
+// decodedList .forEach((element) {
+//   if(element != null){
+//     filesList.add(element);
+//   }
+// });
+
+// calloc.free(pFiles);
+    calloc.free(pBookPath);
+    calloc.free(pTargetPath);
+    return [];
+// return filesList;
+  } catch (e) {
+    calloc.free(pBookPath);
+    calloc.free(pTargetPath);
+    return [];
+  }
+}
